440ba31a274c215c7b1ff4cc735afd7d39f9fa4f
Hemant Kumar <hemantkrmmt@users.noreply.github.com>
2020-08-02 18:19:33 +0530

    add check default route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,5 +19,9 @@ module.exports=app=>{
     app.get('/api/current_user',(req, res)=>{
         res.send(req.user);
     })
+
+    app.get('*',(req, res)=>{
+        res.send("hello world");
+    })
 }
 
